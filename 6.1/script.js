8a6687ac93d95df82cd3d5d2b31d180a3a922e60
IlliaRomanuk <dvgtge@gmail.com>
2025-10-14 00:16:39 +0300

    remade 6.1

--- 6.1/script.js
+++ 6.1/script.js
@@ -1,3 +1,9 @@
-const usersData = {name: 'igor', age: 20, place: 'Odesa'};
-const userInfo = Object.entries(usersData);
-console.log(userInfo);
+const usersData = {
+    name: 'igor', 
+    age: 20, 
+    place: 'Odesa',
+    getInfo () {
+      console.log(`name:${usersData.name}, age: ${usersData.age}, plase: ${usersData.place}`)
+    }
+  };
+  usersData.getInfo();
