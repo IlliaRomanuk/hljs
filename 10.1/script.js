5a65dd320d7d88d64066557f15f7783a7c6e35b9
IlliaRomanuk <dvgtge@gmail.com>
2025-10-23 19:53:45 +0300

    10

---
+++ 10.1/script.js
@@ -0,0 +1,34 @@
+function getFiniteValue(company) {
+  let salaries = [];
+  let money = 0;
+  getProp(company);
+
+  function getProp(o) {
+    for (var prop in o) {
+      if (typeof o[prop] === "object") {
+        getProp(o[prop]);
+      } else if (prop === "salary") {
+        salaries.push(o[prop]);
+      }
+    }
+  }
+  console.log(salaries);
+  for (let i = 0; i < salaries.length; i++) {
+    money += salaries[i];
+  }
+  console.log(money);
+}
+let company = {
+  sales: [
+    { name: "John", salary: 1000 },
+    { name: "Alice", salary: 600 },
+  ],
+  development: {
+    web: [
+      { name: "Peter", salary: 2000 },
+      { name: "Alex", salary: 1800 },
+    ],
+    internals: [{ name: "Jack", salary: 1300 }],
+  },
+};
+getFiniteValue(company);
