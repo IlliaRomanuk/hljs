2e778ea7f1dff9fbae705b6baffea01ef236a573
IlliaRomanuk <dvgtge@gmail.com>
2025-11-06 12:06:27 +0200

    remake 13

--- 13.1/script.js
+++ 13.1/script.js
@@ -8,57 +8,53 @@ const nameMessage = document.querySelector("#nameMessage");
 const nameNumber = document.querySelector("#nameNumber");
 const nameEmail = document.querySelector("#nameEmail");
 const result = {
-    name: false,
-    message: false,
-    number: false,
-    email: false
+  name: false,
+  message: false,
+  number: false,
+  email: false,
 };
-userName.addEventListener("change", function (event) {
-  const value = event.target.value;
-  if (value === "") {
-    nameText.style.display = "block";
-    result.name = false;
-  }
-  else{
-    result.name = true;
-    nameNumber.style.display = "none";
-  }
+const formObj = [
+  {
+    selector: userName,
+    pattern: "",
+    errorSelector: nameText,
+    element: "name",
+  },
+  {
+    selector: userMessage,
+    pattern: /\w{5,}/,
+    errorSelector: nameMessage,
+    element: "message",
+  },
+  {
+    selector: userNumber,
+    pattern: /^\+380\d{9}$/,
+    errorSelector: nameNumber,
+    element: "number",
+  },
+  {
+    selector: userEmail,
+    pattern: /\w{1,}@\w{5}\.\w{3}/,
+    errorSelector: nameEmail,
+    element: "email",
+  },
+];
+formObj.forEach(({ selector, pattern, errorSelector, element }) => {
+  selector.addEventListener("input", function (event) {
+    const value = event.target.value;
+    if (pattern.test(value)) {
+      errorSelector.style.display = "none";
+      result[element] = true;
+    } else {
+      errorSelector.style.display = "block";
+      result[element] = false;
+    }
+  });
 });
-userMessage.addEventListener("change", function (event) {
-  const value = event.target.value;
-  const regex = /\w{5,}/;
-  if (regex.test(value)) {
-    result.message = true
-    nameNumber.style.display = "none";
-  } else{
-    nameMessage.style.display = "block";
-    result.message = false;
-  }
-});
-userNumber.addEventListener("change", function (event) {
-  const value = event.target.value;
-  const regex = /^\+380\d{9}$/;
-  if (regex.test(value)) {
-    result.number = true;
-    nameNumber.style.display = "none";
-  } else {
-    nameNumber.style.display = "block";
-    result.number = false;
-  }
-});
-userEmail.addEventListener("change", function (event) {
-  const value = event.target.value;
-  const regex = /\w{1,}@\w{5}\.\w{3}/;
-  if (regex.test(value)) {
-    result.email = true;
-    nameNumber.style.display = "none";
-  } else {
-    nameEmail.style.display = "block";
-    result.email = false;
-  }
-});
-form.addEventListener("submit", function (event) {
+
+form.addEventListener("submit", (event) => {
   event.preventDefault();
+
   if (!result.name || !result.message || !result.number || !result.email) {
     console.log("error: not all fields are valid");
   } else {
